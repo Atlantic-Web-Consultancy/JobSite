a85113df4ee00ce3e80af5434be453e050895480
Eddie <elremolino91@gmail.com>
2021-05-26 08:27:30 -1000

    styles fixed

--- src/components/ed-componenets/Landing.jsx
+++ src/components/ed-componenets/Landing.jsx
@@ -4,7 +4,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import image from '../../dist/fourthOne.jpeg';
+
 
 
 function Landing() {
