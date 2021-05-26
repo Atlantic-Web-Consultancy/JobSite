1bfd43206d38b13c74b9cb93b4d7b1681fb2c1b7
Grant Schaeffer <ghuakina@gmail.com>
2021-05-26 16:14:20 -0700

    Update TopNav.jsx

--- src/components/topnav/TopNav.jsx
+++ src/components/topnav/TopNav.jsx
@@ -1,20 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
+import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Dropdown from 'react-bootstrap/Dropdown';
+import { LinkContainer } from 'react-router-bootstrap';
+import CalendarView from '../calendar/CalendarView.jsx';
 
-const TopNav = (props) => (
-  <Navbar id="main-nav" fixed="top" bg="dark" expand="lg">
-    <Navbar.Brand id="nav-logo" href="#home">JobSite.</Navbar.Brand>
-    <Dropdown>
-      <Dropdown.Toggle id="dropdown">Dropdown</Dropdown.Toggle>
+const TopNav = props => {
+  const [display, setDisplay] = useState(false);
 
-      <Dropdown.Menu id="dropdown-menu">
-        <Dropdown.Item className="text-light">Home</Dropdown.Item>
-        <Dropdown.Item className="text-light">Calendar</Dropdown.Item>
-        <Dropdown.Item className="text-light">Notes</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-  </Navbar>
-);
+  const toggleCal = (toggleVal) => {
+    setDisplay(toggleVal);
+  };
+
+  const changeNotesView = (view) => {
+    props.changeNotesView(view);
+  };
+
+  if (props.navItems.includes('Landing')) {
+    return (
+      <Navbar id="main-nav" fixed="top">
+        <Nav>
+          <LinkContainer to="/">
+            <Navbar.Brand id="nav-logo">JobSite.</Navbar.Brand>
+          </LinkContainer>
+        </Nav>
+        <Dropdown id="dropdown">
+          <Dropdown.Toggle id="dropdown-button" />
+
+          <Dropdown.Menu id="dropdown-menu">
+            <LinkContainer to="/login/logout">
+              <Dropdown.Item>Login/Logout</Dropdown.Item>
+            </LinkContainer>
+          </Dropdown.Menu>
+        </Dropdown>
+      </Navbar>
+    );
+  }
+  return (
+    <div>
+      <Navbar id="main-nav" fixed="top">
+        <Nav>
+          <LinkContainer to="/">
+            <Navbar.Brand id="nav-logo">JobSite.</Navbar.Brand>
+          </LinkContainer>
+        </Nav>
+        <Dropdown id="dropdown">
+          <Dropdown.Toggle id="dropdown-button" />
+
+          <Dropdown.Menu id="dropdown-menu">
+            <LinkContainer to="Home">
+              <Dropdown.Item>Home</Dropdown.Item>
+            </LinkContainer>
+            <Dropdown.Item onClick={() => toggleCal(true)}>Calendar</Dropdown.Item>
+            <Dropdown.Item onClick={() => changeNotesView(true)}>Notes</Dropdown.Item>
+            {props.navItems.map(item => (
+              <LinkContainer to={`/${item.split(' ').join('').toLowerCase()}`}>
+                <Dropdown.Item>{item}</Dropdown.Item>
+              </LinkContainer>
+            ))}
+          </Dropdown.Menu>
+        </Dropdown>
+      </Navbar>
+      <CalendarView toggleCal={toggleCal} display={display} />
+    </div>
+  );
+};
 
 export default TopNav;
