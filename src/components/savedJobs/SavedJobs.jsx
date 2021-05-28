036468051cc110dda0164ad54f2c3c8ae8f60d8e
Praneeth Gogineni <praneeth.gogineni@gmail.com>
2021-05-28 06:39:42 -0700

    Change saved jobs to be class component

--- src/components/savedJobs/SavedJobs.jsx
+++ src/components/savedJobs/SavedJobs.jsx
@@ -1,109 +1,118 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
 import TopNav from '../topNav/TopNav.jsx';
 import Parse from './Parse.js';
 
-const SavedJobs = props => {
-  const [key, setKey] = useState('saved');
-  Parse.login((data) => console.log(data));
-
-  const filterApps = (apps) => {
-    console.log(apps);
+class SavedJobs extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      apps: {},
+      showApps: false,
+      key: 'saved'
+    }
   }
 
-  return (
-    <div>
-      <div id="tabs">
-        <Tabs id="tab-content"
-          activeKey={key}
-          onSelect={(k) => setKey(k)}
-        >
-          <Tab eventKey="saved" title="Saved">
-            <div className="tabs">
-              <h2>Job Title</h2>
-              <section>Description</section>
-              <section>Status</section>
-            </div>
-            <div className="tabs">
-              <h2>Job Title</h2>
-              <section>Description</section>
-              <section>Status</section>
-            </div>
-            <div className="tabs">
-              <h2>Job Title</h2>
-              <section>Description</section>
-              <section>Status</section>
-            </div>
-            <div className="tabs">
-              <h2>Job Title</h2>
-              <section>Description</section>
-              <section>Status</section>
-            </div>
-            <div className="tabs">
-              <h2>Job Title</h2>
-              <section>Description</section>
-              <section>Status</section>
-            </div>
-          </Tab>
-          <Tab eventKey="applied" title="Applied">
-            <div className="tabs">
-              <h2>Job Title</h2>
-              <section>Description</section>
-              <section>Status</section>
-            </div>
-            <div className="tabs">
-              <h2>Job Title</h2>
-              <section>Description</section>
-              <section>Status</section>
-            </div>
-            <div className="tabs">
-              <h2>Job Title</h2>
-              <section>Description</section>
-              <section>Status</section>
-            </div>
-            <div className="tabs">
-              <h2>Job Title</h2>
-              <section>Description</section>
-              <section>Status</section>
-            </div>
-            <div className="tabs">
-              <h2>Job Title</h2>
-              <section>Description</section>
-              <section>Status</section>
-            </div>
-          </Tab>
-          <Tab eventKey="interviewed" title="Interviewed">
-            <div className="tabs">
-              <h2>Job Title</h2>
-              <section>Description</section>
-              <section>Status</section>
-            </div>
-            <div className="tabs">
-              <h2>Job Title</h2>
-              <section>Description</section>
-              <section>Status</section>
-            </div>
-            <div className="tabs">
-              <h2>Job Title</h2>
-              <section>Description</section>
-              <section>Status</section>
-            </div>
-            <div className="tabs">
-              <h2>Job Title</h2>
-              <section>Description</section>
-              <section>Status</section>
-            </div>
-            <div className="tabs">
-              <h2>Job Title</h2>
-              <section>Description</section>
-              <section>Status</section>
-            </div>
-          </Tab>
-        </Tabs>
+  componentDidMount() {
+    Parse.login((data) => setApps(data));
+  }
+
+
+  render () {
+    return (
+      <div>
+        <div id="tabs">
+          <Tabs id="tab-content"
+            activeKey={this.state.key}
+            onSelect={(k) => this.setState({key: k})}
+          >
+            <Tab eventKey="saved" title="Saved">
+              <div className="tabs">
+                <h2>Job Title</h2>
+                <section>Description</section>
+                <section>Status</section>
+              </div>
+              <div className="tabs">
+                <h2>Job Title</h2>
+                <section>Description</section>
+                <section>Status</section>
+              </div>
+              <div className="tabs">
+                <h2>Job Title</h2>
+                <section>Description</section>
+                <section>Status</section>
+              </div>
+              <div className="tabs">
+                <h2>Job Title</h2>
+                <section>Description</section>
+                <section>Status</section>
+              </div>
+              <div className="tabs">
+                <h2>Job Title</h2>
+                <section>Description</section>
+                <section>Status</section>
+              </div>
+            </Tab>
+            <Tab eventKey="applied" title="Applied">
+              <div className="tabs">
+                <h2>Job Title</h2>
+                <section>Description</section>
+                <section>Status</section>
+              </div>
+              <div className="tabs">
+                <h2>Job Title</h2>
+                <section>Description</section>
+                <section>Status</section>
+              </div>
+              <div className="tabs">
+                <h2>Job Title</h2>
+                <section>Description</section>
+                <section>Status</section>
+              </div>
+              <div className="tabs">
+                <h2>Job Title</h2>
+                <section>Description</section>
+                <section>Status</section>
+              </div>
+              <div className="tabs">
+                <h2>Job Title</h2>
+                <section>Description</section>
+                <section>Status</section>
+              </div>
+            </Tab>
+            <Tab eventKey="interviewed" title="Interviewed">
+              <div className="tabs">
+                <h2>Job Title</h2>
+                <section>Description</section>
+                <section>Status</section>
+              </div>
+              <div className="tabs">
+                <h2>Job Title</h2>
+                <section>Description</section>
+                <section>Status</section>
+              </div>
+              <div className="tabs">
+                <h2>Job Title</h2>
+                <section>Description</section>
+                <section>Status</section>
+              </div>
+              <div className="tabs">
+                <h2>Job Title</h2>
+                <section>Description</section>
+                <section>Status</section>
+              </div>
+              <div className="tabs">
+                <h2>Job Title</h2>
+                <section>Description</section>
+                <section>Status</section>
+              </div>
+            </Tab>
+          </Tabs>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default SavedJobs;
